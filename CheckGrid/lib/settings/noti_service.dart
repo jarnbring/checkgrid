8bb049222d2c312de89b3a3ff628e22c246b95fd
jarnbring <william@jarnbring.se>
2025-05-05 00:35:55 +0200

    Added scheduled notifications!

--- CheckGrid/lib/settings/noti_service.dart
+++ CheckGrid/lib/settings/noti_service.dart
@@ -1,4 +1,10 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest.dart';
+import 'package:timezone/data/latest_all.dart' as tz show initializeTimeZones;
+import 'package:timezone/timezone.dart' as tz;
+import 'package:flutter_timezone/flutter_timezone.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 
 class NotiService {
   final notificationsPlugin = FlutterLocalNotificationsPlugin();
@@ -10,6 +16,11 @@ class NotiService {
   // INITIALIZE
   Future<void> initNotification() async {
     if (_isInitialized) return; // prevent re-initialization
+
+    // init timezone handling
+    tz.initializeTimeZones();
+    final String currentTimeZone = await FlutterTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(currentTimeZone));
 
     // prepare android init settings
     const initSettingsAndroid = AndroidInitializationSettings(
@@ -54,4 +65,56 @@ class NotiService {
   }) async {
     return notificationsPlugin.show(id, title, body, notificationDetails());
   }
+
+  /*
+  
+  Schedule a notification at a specified time (e.g. 11pm)
+
+  - hour (0-23)
+  - minute (0-59)
+  */
+
+  Future<void> scheduleNotification({
+    int id = 1,
+    required String title,
+    required String body,
+    required int hour,
+    required int minute,
+  }) async {
+    // Get the current date/time in device's local timezone
+    final now = tz.TZDateTime.now(tz.local);
+
+    // Create a date/time for today at the specified hour/min
+    var scheduleDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      hour,
+      minute,
+    );
+
+    // Schedule the notification
+    await notificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      scheduleDate,
+      notificationDetails(),
+      
+      // Android specific: Allow notification while device is in low-power mode
+      androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+
+      // Make notification repeat DAILY at same time
+      matchDateTimeComponents: DateTimeComponents.time
+    );
+
+    print("Notification scheduled!: $scheduleDate");
+  }
+
+  // Cancel all notifications
+  Future<void> cancelAllNotifications() async {
+    await notificationsPlugin.cancelAll();
+  }
+
 }
