5a50f63db0833dd5c6c25d9ca0df8218cb606d8e
Frida Jarnbring <fatnigga@MacBook-Pro-som-tillhor-Frida.local>
2025-06-06 00:57:44 +0200

    Added revive ads

--- CheckGrid/lib/pages/menu_page.dart
+++ CheckGrid/lib/pages/menu_page.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:gamename/banner_ad.dart';
+import 'package:gamename/ads/banner_ad.dart';
 import 'package:gamename/pages/feedback_page.dart';
 import 'package:gamename/pages/game_page.dart';
 import 'package:gamename/pages/store_page.dart';
